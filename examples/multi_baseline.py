4b9f900bd882525c85120c1fe4b87eb83ecf7aa4
Calvin Kinateder <calvinkinateder@gmail.com>
2024-10-02 15:29:21 -0400

    updated baseline

--- examples/multi_baseline.py
+++ examples/multi_baseline.py
@@ -20,6 +20,7 @@ from time import time
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+SEED = 42
 
 percentiles = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99]
 
@@ -43,6 +44,7 @@ def run_cifar10(
     weighted_clauses=False,
     epochs=60,
     type_i_ii_ratio=1.0,
+    clause_drop_p=0.0,
 ):
     args = DotDict(
         {
@@ -54,6 +56,7 @@ def run_cifar10(
             "weighted_clauses": weighted_clauses,
             "epochs": epochs,
             "type_i_ii_ratio": type_i_ii_ratio,
+            "clause_drop_p": clause_drop_p,
         }
     )
     _LOGGER.info(f"Running CIFAR10 with {args}")
@@ -102,6 +105,8 @@ def run_cifar10(
         platform=args.device,
         weighted_clauses=args.weighted_clauses,
         type_i_ii_ratio=args.type_i_ii_ratio,
+        seed=SEED,
+        clause_drop_p=args.clause_drop_p,
     )
 
     _LOGGER.info(f"Running {TMClassifier} for {args.epochs}")
@@ -125,90 +130,6 @@ def run_cifar10(
             CudaProfiler().print_timings(benchmark=benchmark_total)
 
 
-def run_cifar100(
-    num_clauses: int = 80000,
-    T: int = int(80000//10*0.75),
-    platform:str = "CUDA",
-    s=10.0,
-    patch_size=3,
-    resolution=8,
-    number_of_state_bits_ta=10,
-    literal_drop_p=0.0,
-    epochs=250,
-    ensembles=10,
-):
-    args = DotDict(
-        {
-            "num_clauses": num_clauses,
-            "T": T,
-            "s": s,
-            "patch_size": patch_size,
-            "resolution": resolution,
-            "number_of_state_bits_ta": number_of_state_bits_ta,
-            "literal_drop_p": literal_drop_p,
-            "epochs": epochs,
-            "ensembles": ensembles,
-            "platform": platform,
-        }
-    )
-    _LOGGER.info(f"Running CIFAR100 with {args}")
-    (X_train_org, Y_train), (X_test_org, Y_test) = cifar100.load_data()
-
-    Y_train = Y_train.reshape(Y_train.shape[0])
-    Y_test = Y_test.reshape(Y_test.shape[0])
-
-    X_train = np.empty((X_train_org.shape[0], X_train_org.shape[1],
-                       X_train_org.shape[2], X_train_org.shape[3], args.resolution), dtype=np.uint8)
-    for z in range(args.resolution):
-        X_train[:, :, :, :, z] = X_train_org[:,
-                                             :, :, :] >= (z+1)*255/(args.resolution+1)
-
-    X_test = np.empty((X_test_org.shape[0], X_test_org.shape[1],
-                      X_test_org.shape[2], X_test_org.shape[3], args.resolution), dtype=np.uint8)
-    for z in range(args.resolution):
-        X_test[:, :, :, :, z] = X_test_org[:,
-                                           :, :, :] >= (z+1)*255/(args.resolution+1)
-
-    X_train = X_train.reshape(
-        (X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], 3*args.resolution))
-    X_test = X_test.reshape(
-        (X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], 3*args.resolution))
-
-    #f = open("cifar100_%.1f_%d_%d_%d_%.2f_%d.txt" %
-    #         (s, args.num_clauses, args.T,  patch_size, literal_drop_p, args.resolution), "w+")
-    for e in range(args.ensembles):
-        tm = TMCoalescedClassifier(args.num_clauses, args.T, s, platform=args.platform, patch_dim=(
-            args.patch_size, args.patch_size), number_of_state_bits_ta=args.number_of_state_bits_ta, focused_negative_sampling=True, weighted_clauses=True, literal_drop_p=args.literal_drop_p)
-        for i in range(args.epochs):
-            start_training = time()
-            tm.fit(X_train, Y_train)
-            stop_training = time()
-
-            start_testing = time()
-            result_test = 100*(tm.predict(X_test) == Y_test).mean()
-            stop_testing = time()
-
-            result_train = 100*(tm.predict(X_train) == Y_train).mean()
-            _LOGGER.info("%d %d %.2f %.2f %.2f %.2f" % (e, i, result_train, result_test,
-                  stop_training-start_training, stop_testing-start_testing))
-            #print("%d %d %.2f %.2f %.2f %.2f" % (e, i, result_train, result_test,
-            #      stop_training-start_training, stop_testing-start_testing), file=f)
-            #f.flush()
-    #f.close()
-
-"""
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--num_clauses", default=2000, type=int)
-parser.add_argument("--T", default=5000, type=int)
-parser.add_argument("--s", default=10.0, type=float)
-parser.add_argument("--max_included_literals", default=32, type=int)
-parser.add_argument("--platform", default="CPU", type=str, choices=["CPU", "CPU_sparse", "CUDA"])
-parser.add_argument("--weighted_clauses", default=True, type=bool)
-parser.add_argument("--epochs", default=60, type=int)
-args = parser.parse_args()
-"""
-
 def metrics(args):
     return dict(
         accuracy=[],
@@ -224,6 +145,7 @@ def run_mnist(
     platform="CUDA",
     weighted_clauses=True,
     epochs=60,
+    clause_drop_p=0.0,
 ):
     args = DotDict(
         {
@@ -234,7 +156,7 @@ def run_mnist(
             "platform": platform,
             "weighted_clauses": weighted_clauses,
             "epochs": epochs,
-        }
+            "clause_drop_p": clause_drop_p        }
     )
     _LOGGER.info(f"Running MNIST with {args}")
     experiment_results = metrics(args)
@@ -248,7 +170,8 @@ def run_mnist(
         max_included_literals=args.max_included_literals,
         platform=args.platform,
         weighted_clauses=args.weighted_clauses,
-        seed=42,
+        seed=SEED,
+        clause_drop_p=args.clause_drop_p,
     )
 
     _LOGGER.info(f"Running {TMClassifier} for {args.epochs}")
@@ -304,35 +227,65 @@ def run_fashion_mnist(
             "platform": platform,
         }
     )
+    experiment_results = metrics(args)
     _LOGGER.info(f"Running FashionMNIST with {args}")
     (X_train_org, Y_train), (X_test_org, Y_test) = fashion_mnist.load_data()
 
     Y_train=Y_train.reshape(Y_train.shape[0])
     Y_test=Y_test.reshape(Y_test.shape[0])
 
-    X_train = np.empty((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], resolution), dtype=np.uint8)
-    for z in range(resolution):
-            X_train[:,:,:,z] = X_train_org[:,:,:] >= (z+1)*255/(resolution+1)
+    X_train = np.empty((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], args.resolution), dtype=np.uint8)
+    for z in range(args.resolution):
+            X_train[:,:,:,z] = X_train_org[:,:,:] >= (z+1)*255/(args.resolution+1)
 
-    X_test = np.empty((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], resolution), dtype=np.uint8)
-    for z in range(resolution):
-            X_test[:,:,:,z] = X_test_org[:,:,:] >= (z+1)*255/(resolution+1)
+    X_test = np.empty((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], args.resolution), dtype=np.uint8)
+    for z in range(args.resolution):
+            X_test[:,:,:,z] = X_test_org[:,:,:] >= (z+1)*255/(args.resolution+1)
 
-    X_train = X_train.reshape((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], resolution))
-    X_test = X_test.reshape((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], resolution))
+    X_train = X_train.reshape((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], args.resolution))
+    X_test = X_test.reshape((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], args.resolution))
 
-    tm = TMCoalescedClassifier(num_clauses, T, s, clause_drop_p=clause_drop_p, patch_dim=(patch_size, patch_size), platform=platform, weighted_clauses=True)
-    for i in range(epochs):
-            start_training = time()
-            tm.fit(X_train, Y_train)
-            stop_training = time()
+    tm = TMClassifier(args.num_clauses, args.T, args.s, clause_drop_p=args.clause_drop_p, patch_dim=(args.patch_size, args.patch_size), platform=args.platform, weighted_clauses=True)
+    for epoch in range(epochs):
+        """
+        start_training = time()
+        tm.fit(X_train, Y_train)
+        stop_training = time()
 
-            start_testing = time()
-            result_test = 100*(tm.predict(X_test) == Y_test).mean()
-            stop_testing = time()
+        start_testing = time()
+        result_test = 100*(tm.predict(X_test) == Y_test).mean()
+        stop_testing = time()
 
-            result_train = 100*(tm.predict(X_train) == Y_train).mean()
-            _LOGGER.info(f"Epoch: {i + 1}, Accuracy: {result_train:.2f}, Training Time: {stop_training-start_training:.2f}s, Testing Time: {stop_testing-start_testing:.2f}s")
+        result_train = 100*(tm.predict(X_train) == Y_train).mean()
+        _LOGGER.info(f"Epoch: {epoch + 1}, Accuracy: {result_train:.2f}, Training Time: {stop_training-start_training:.2f}s, Testing Time: {stop_testing-start_testing:.2f}s")
+        """
+
+        benchmark_total = BenchmarkTimer(logger=None, text="Epoch Time")
+        with benchmark_total:
+            benchmark1 = BenchmarkTimer(logger=None, text="Training Time")
+            with benchmark1:
+                res = tm.fit(
+                    X_train, Y_train,
+                    metrics=["update_p"],
+                )
+
+            experiment_results["train_time"].append(benchmark1.elapsed())
+
+            # print(res)
+            benchmark2 = BenchmarkTimer(logger=None, text="Testing Time")
+            with benchmark2:
+                result = 100 * (tm.predict(X_test) == Y_test).mean()
+                experiment_results["accuracy"].append(result)
+            experiment_results["test_time"].append(benchmark2.elapsed())
+
+            _LOGGER.info(f"Epoch: {epoch + 1}, Accuracy: {result:.2f}, Training Time: {benchmark1.elapsed():.2f}s, "
+                         f"Testing Time: {benchmark2.elapsed():.2f}s")
+
+        if args.platform == "CUDA":
+            CudaProfiler().print_timings(benchmark=benchmark_total)
+
+    return experiment_results
+
 
 
 
